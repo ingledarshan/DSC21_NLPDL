ce933c4163206eba76f2adaa9a1a42c23e1cdad2
ingledarshan <56253833+ingledarshan@users.noreply.github.com>
2021-06-06 14:06:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_features = [ int(x) for x in request.form.values() ]
+    final_features = [ np.array(int_features) ]
+    prediction = model.predict(final_features)
+    
+    output = round(prediction[0], 2)
+    
+    return render_template('index.html', prediction_text= 'Employee Salary should be $ {}'.format(output))
+
+if __name__=='__main__':
+    app.run(debug=True)
+
